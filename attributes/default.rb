fe99f816c820597837b2148f2b39a4383b0b5e23
khayredinov <dh@finch-melrose.com>
2014-12-27 18:31:41 +0300

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 default['grafana']['install_flavour']= 'release'
 default['grafana']['release_version']= '1.6.1'
-default['grafana']['revision'] = "810f46c450c9582de763395c2edc4f209abc3632"
+default['grafana']['revision'] = "f5e8f9334be707c54f2080a2df3cb0f20e4d995a"
 default['grafana']['reference'] = "master"
 default['grafana']['install_dir'] = "/opt/grafana"
-default['grafana']['repo'] = "https://github.com/torkelo/grafana.git"
+default['grafana']['repo'] = "https://github.com/grafana/grafana.git"
 default['grafana']['elasticsearch_url'] = '"http://"+window.location.hostname+":9200"'
 default['grafana']['datasources'] = {
 	
